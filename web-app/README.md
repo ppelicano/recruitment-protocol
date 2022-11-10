8777ad7af2b7df556bc0adf0306427221ade2057
Pedro Pelicano <pedro.pelicano@lynkco.com>
2022-11-10 09:26:16 +0000

    adds ens and recruitment contracts

--- web-app/README.md
+++ web-app/README.md
@@ -16,5 +16,7 @@ Components:
   - jobFinalize(jobTransactionHash, candidateAddress, refererAddress) { enablerAddress guard }
         notes: 
 
-
+Goerli Smart Contracts:
+- Recruitment: 0xDFfA6230381bc0280d3a585Cc33e0e3B9D87b55C
+- EnsSubdomainFactory: 0x2dD32Ca2726cc86ba40625c2D6B7452CE5808dCa
 
--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import Web3Modal from 'web3modal'
 import Web3 from 'web3'
+import EnsSubdomainFactory from './ABIS/EnsSubdomainFactory.json'
+import Recruitment from './ABIS/Recruitment.json'
 import './App.css';
 
 function App() {
@@ -8,6 +10,8 @@ function App() {
   const [web3, setWeb3] = useState(null)
   const [provider, setProvider] = useState(null)
   const [address, setAddress] = useState(null)
+  const [contract_EnsSubdomainFactory, setContract_EnsSubdomainFactory] = useState(null)
+  const [contract_Recruitment, setContract_Recruitment] = useState(null)
   useEffect(()=>{
     setWeb3Modal(new Web3Modal({
       network: 'goerli', // optional
@@ -22,8 +26,8 @@ function App() {
   
   useEffect(()=> {
     if (provider) {
-      provider.on("accountsChanged", (accounts) => {
-        console.log("accountsChanged", accounts)
+      provider.on('accountsChanged', (accounts) => {
+        console.log('accountsChanged', accounts)
         if (!accounts?.length) {
           setProvider(null)
           setWeb3(null)
@@ -34,59 +38,40 @@ function App() {
       });
       
       // Subscribe to chainId change
-      provider.on("chainChanged", (chainId) => {
-        console.log("chainChanged", chainId);
+      provider.on('chainChanged', (chainId) => {
+        console.log('chainChanged', chainId);
       });
       
       // Subscribe to provider connection
-      provider.on("connect", (info) => {
-        console.log("connect",info);
+      provider.on('connect', (info) => {
+        console.log('connect',info);
       });
       
       // Subscribe to provider disconnection
-      provider.on("disconnect", (error) => {
-        console.log("disconnect",error);
+      provider.on('disconnect', (error) => {
+        console.log('disconnect',error);
       });
     }
     
     setWeb3(provider ? new Web3(provider) : null)
   },[provider])
 
+  useEffect(()=>{
+    if (web3) {
+      setContract_EnsSubdomainFactory(new web3.eth.Contract(EnsSubdomainFactory.abi, '0x2dD32Ca2726cc86ba40625c2D6B7452CE5808dCa'))
+      setContract_Recruitment(new web3.eth.Contract(Recruitment.abi, '0xDFfA6230381bc0280d3a585Cc33e0e3B9D87b55C'))
+    }
+  },[web3])
+
   const connectWallet = async () => {
     const _provider = await web3Modal.connect()
     _provider && setProvider(_provider)
   }
 
-  const createSecryptSubdomain = async () => {
-    // setNewSubdomainReceipt({loading: true})
-    // const txOptions = {
-    //   nonce: web3.utils.toHex(web3.eth.getTransactionCount(account)),
-    //   //gasLimit: web3.utils.toHex(3000000),
-    //   gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
-    //   //gas: 1000000,
-    //   from: account
-    // }
-    // secryptContract && secryptContract.methods.newSubdomain(account, account).send(txOptions)
-    //       .then((txRaw) => {
-    //         console.log("THEN => ", txRaw)
-    //         setNewSubdomainReceipt({txHash: txRaw.transactionHash})
-    //       })
-    //       .catch((error)=> {
-    //         Object.keys(error).forEach(item => {
-    //           console.log("ERROR CREATING SECRYPT SUBDOMAIN", error)
-    //           if (item === "receipt" && error?.receipt?.transactionHash) {
-    //             setNewSubdomainReceipt({errorHash: error.receipt.transactionHash})
-    //           }
-    //           else {
-    //             setNewSubdomainReceipt({error: error})
-    //           }
-    //         })
-    //       })
-  }
-
-console.log("web3", web3?.eth?.accounts)
+  console.log('contract_EnsSubdomainFactory', contract_EnsSubdomainFactory)
+  console.log('contract_Recruitment', contract_Recruitment)
   return (
-    <div className="App">
+    <div className='App'>
       {!web3 && <button onClick={()=>{connectWallet()}}>Connect to your wallet!</button>}
       {web3 && <h1>Welcome you are logged in!</h1>}
     </div>
