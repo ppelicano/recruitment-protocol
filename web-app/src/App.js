db8d49e2bbe0b2224af08e9d3acae77057ae1768
Pedro Pelicano <pedro.pelicano@lynkco.com>
2022-11-25 21:40:40 +0000

    happy flow achieved for initialDeposit method

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,15 +1,18 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react'
 import Web3Modal from 'web3modal'
 import Web3 from 'web3'
-import EnsSubdomainFactory from './ABIS/EnsSubdomainFactory.json'
-import Recruitment from './ABIS/Recruitment.json'
-import MinterMock from './ABIS/MinterMock.json'
+import abi from './abi.json'
 import ContractAddresses from './contract-addresses.json'
 import './App.css';
 
+const MinterMock = abi.output.contracts["contracts/MinterMock.sol"].MinterMock.abi
+const Recruitment = abi.output.contracts["contracts/Recruitment.sol"].Recruitment.abi
+const EnsSubdomainFactory = abi.output.contracts["contracts/EnsSubdomainFactory.sol"].EnsSubdomainFactory.abi
+console.log(MinterMock)
+console.log(Recruitment)
+console.log(EnsSubdomainFactory)
+
 function App() {
-  const input_whitelist_token_contract = useRef(null);
-  const input_whitelist_token_symbol = useRef(null);
   const [web3Modal, setWeb3Modal] = useState(null)
   const [web3, setWeb3] = useState(null)
   const [currentAccount, setCurrentAccount] = useState(null)
@@ -17,6 +20,7 @@ function App() {
   const [address, setAddress] = useState(null)
   const [contract_EnsSubdomainFactory, setContract_EnsSubdomainFactory] = useState(null)
   const [contract_Recruitment, setContract_Recruitment] = useState(null)
+  const [contract_MinterMock, setContract_MinterMock] = useState(null)
   useEffect(()=>{
     setWeb3Modal(new Web3Modal({
       network: 'goerli', // optional
@@ -66,24 +70,15 @@ function App() {
 
   useEffect(()=>{
     if (web3) {
-      setContract_EnsSubdomainFactory(new web3.eth.Contract(EnsSubdomainFactory.abi, ContractAddresses.EnsSubdomainFactory))
-      setContract_Recruitment(new web3.eth.Contract(Recruitment.abi, ContractAddresses.Recruitment))
-      // const daiContract = new web3.eth.Contract(MinterMock.abi, ContractAddresses.MinterMock)
-      // console.log("daiContract", daiContract, ContractAddresses.MinterMock)
-      // daiContract.methods.balanceOf(currentAccount).call().then(result => {
-      //    console.log("daiContract balanceOf",result)
-      //  })
+      setContract_EnsSubdomainFactory(new web3.eth.Contract(EnsSubdomainFactory, ContractAddresses.EnsSubdomainFactory))
+      setContract_Recruitment(new web3.eth.Contract(Recruitment, ContractAddresses.Recruitment))
+      setContract_MinterMock(new web3.eth.Contract(MinterMock, ContractAddresses.MinterMock))
       web3.eth.getAccounts().then(accounts => {
         setCurrentAccount(accounts[0])
       })
     }
   },[web3])
 
-  const connectWallet = async () => {
-    const _provider = await web3Modal.connect()
-    _provider && setProvider(_provider)
-  }
-  console.log("currentAccount", currentAccount)
   useEffect(() => {
     if (contract_Recruitment && currentAccount) {
       contract_Recruitment.methods.getWhitelistedTokenAddresses(
@@ -91,46 +86,58 @@ function App() {
       ).call({from: currentAccount}, function(error, result){
           console.log("getWhitelistedTokenAddresses", error, result)
       })
+
+      // contract_Recruitment.methods.whitelistToken(
+      //   web3.utils.asciiToHex('DAI'),
+      //   ContractAddresses.MinterMock
+      // ).send({from: currentAccount }).then(res => { console.log(res) })
+
+
+      contract_MinterMock.methods.balanceOf(currentAccount).call().then(result => {
+          console.log("daiContract balanceOf",result)
+      })
     }
   },[contract_Recruitment, currentAccount])
   
-  const whitelistDaiOnClick = () => {
-    contract_Recruitment.methods.whitelistToken(
-      web3.utils.asciiToHex('DAI'),
-      ContractAddresses.MinterMock
-    ).send({from: currentAccount })
+  const connectWallet = async () => {
+    const _provider = await web3Modal.connect()
+    _provider && setProvider(_provider)
   }
 
-  const whitelistTokenOnClick = () => {
-    console.log(address)
-    console.log("whitelistTokenOnClick", 
-    web3.utils.asciiToHex(input_whitelist_token_symbol.current.value),
-      input_whitelist_token_contract.current.value)
-    contract_Recruitment.methods.whitelistToken(
-      web3.utils.asciiToHex(input_whitelist_token_symbol.current.value),
-      input_whitelist_token_contract.current.value
-    ).send({from: currentAccount })
-    //USDC
-    //0xde637d4c445ca2aae8f782ffac8d2971b93a4998
-    //6
-    //0x55534443
-    //USDT
-    //0x509Ee0d083DdF8AC028f2a56731412edD63223B9
-    //6
-    //0x55534454
-
-    //DAI
-    //0x73967c6a0904aA032C103b4104747E88c566B1A2
+  const getGasAmountFor_Recruitment_initialDepositDAI = async () => {
+    return await contract_Recruitment.methods.initialDeposit(1000, web3.utils.asciiToHex("DAI Mock")).estimateGas({ from: currentAccount });
   }
 
   const initialDepositOnClick = async () => {
-    // contract_Recruitment.methods.depositUsd(
-    //   1,
-    //   web3.utils.asciiToHex('USDC'),
-    // ).send({from: currentAccount })
+    let txCount = await web3.eth.getTransactionCount(currentAccount),
+      decimals = await contract_MinterMock.methods.decimals().call({ from: currentAccount }),
+      value = `1000${'0'.repeat(decimals)}`
+    console.log("decimals",decimals)
+    console.log("txCount", txCount, await web3.eth.getGasPrice())
+    const txObject = {
+      nonce: web3.utils.toHex(txCount),
+      from: currentAccount,
+      //gasPrice: web3.utils.toHex(await web3.eth.getGasPrice()),
+      gas:      10000000,
+      //gasLimit: 8000000
+    }
+
+    // const gasAmmount = await getGasAmountFor_Recruitment_initialDepositDAI();
+    // console.log("gasAmmount", gasAmmount);
+    // return;
+
+    console.log(currentAccount);
+    console.log("TokenAddress", await contract_Recruitment.methods.TokenAddress().call({from: currentAccount}))
+    console.log("SenderAddress", await contract_Recruitment.methods.SenderAddress().call({from: currentAccount}))
+    console.log("ThisAddress", await contract_Recruitment.methods.ThisAddress().call({from: currentAccount}))
+
+    
+    let approveReceipt = await contract_MinterMock.methods.approve(ContractAddresses.Recruitment, value).send({ from: currentAccount });
+    console.log("approveReceipt", approveReceipt);
     let response = await contract_Recruitment.methods.initialDeposit(
+      value,
       web3.utils.asciiToHex('DAI')
-    ).send({from: currentAccount });
+    ).send(txObject).then(res => { console.log(res) })
     console.log(response)
   }
 
@@ -140,31 +147,17 @@ function App() {
       {web3 && (
         <div className='form'>
           <h1>Welcome you are logged in! {currentAccount}</h1>
-          <hr/>
-          <h1>Token whitelisting</h1>
-          <div>
-            <input placeholder='token contract' ref={input_whitelist_token_contract} />
-          </div>
-          <div>
-            <input placeholder='token symbol' ref={input_whitelist_token_symbol} />
-          </div>
-          <div>
-            <button onClick={whitelistTokenOnClick}>Whitelist TOKEN</button>
-          </div>
-          <div>
-            <button onClick={whitelistDaiOnClick}>Whitelist DAI Mock</button>
-          </div>
 
           <hr/>
           <h1>Initial Deposit</h1>
           <div>
-            <input placeholder='month1 refund %'  />
+            <input placeholder='month1 refund %' />
           </div>
           <div>
-            <input placeholder='month2 refund %'  />
+            <input placeholder='month2 refund %' />
           </div>
           <div>
-            <input placeholder='month3 refund %'  />
+            <input placeholder='month3 refund %' />
           </div>
           <div>
             <button onClick={initialDepositOnClick}>Deposit</button>
